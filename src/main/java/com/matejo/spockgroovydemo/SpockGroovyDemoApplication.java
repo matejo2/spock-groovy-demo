e61f3e91b79bd08ba5fbeeec88de6025225feb0d
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-09-01 17:11:07 +0200

    initial commit

---
+++ src/main/java/com/matejo/spockgroovydemo/SpockGroovyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.matejo.spockgroovydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockGroovyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockGroovyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matejo/spockgroovydemo/SpockGroovyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matejo.spockgroovydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockGroovyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
