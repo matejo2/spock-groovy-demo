51c45e379d09ab0e298179c9142a24ce65010fef
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-09-01 18:36:21 +0200

    implemented junit test

--- src/test/java/com/matejo/spockgroovydemo/JunitEmployeeControllerTest.java
+++ src/test/java/com/matejo/spockgroovydemo/JunitEmployeeControllerTest.java
@@ -3,25 +3,37 @@ package com.matejo.spockgroovydemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(controllers = EmployeeController.class)
 class JunitEmployeeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private EmployeeService service;
+
     @Test
     void basic_http_get() throws Exception {
         //given
         var url = "/employees";
+        List<Employee> expectedEmployees = new ArrayList<>();
+        expectedEmployees.add(new Employee(1L, "ad", "asd"));
         // when
-        var response = mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().isOk());
+        var response = mockMvc.perform(MockMvcRequestBuilders.get(url))
+                .andExpect(status().isOk());
+
+
+        when(service.getEmployees()).thenReturn(expectedEmployees);
 
     }
 }
