8d18f1916105bbecd6c76374bccf99000a91c9d6
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-09-06 14:26:47 +0200

    added object mapper to groovy test, so we dont need to write the json

--- src/test/groovy/com/matejo/spockgroovydemo/EmployeeControllerTest.groovy
+++ src/test/groovy/com/matejo/spockgroovydemo/EmployeeControllerTest.groovy
@@ -1,5 +1,6 @@
 package com.matejo.spockgroovydemo
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -17,14 +18,13 @@ class EmployeeControllerTest extends Specification {
     @SpringBean
     EmployeeService service = Mock()
 
+    ObjectMapper mapper = new ObjectMapper()
+
     def "get employees returns employees"() {
         given: "the rest api"
         def url = "/employees"
         and: "a list of employees"
         def expectedEmployees = [new Employee(1L, "Hopper", "Grace")]
-        and: "expected json response"
-        def expectedJson = "[{\"id\":1,\"lastName\":\"Hopper\",\"firstName\":\"Grace\"}]"
-
 
         when: "rest api is called"
         def response = mockMvc.perform(MockMvcRequestBuilders.get(url)).andReturn().response
@@ -34,6 +34,6 @@ class EmployeeControllerTest extends Specification {
         and: "employee service was called"
         1 * service.getEmployees() >> expectedEmployees
         and: "response has expected employees"
-        response.getContentAsString() == expectedJson
+        response.getContentAsString() == mapper.writeValueAsString(expectedEmployees)
     }
 }
